9d50c01738a2844a6abc824b6aab93b6872d6311
taylorreichner <taylorreichner@yahoo.com>
2021-02-23 18:05:53 -0800

    its broken for now

--- data/quarterbacks.js
+++ data/quarterbacks.js
@@ -3,31 +3,37 @@ module.exports = [
       'name': 'Tom_Brady',
       'accuracy': 94,
       'is_old': true,
+      'style_id': 1,
   },
   {
       'name': 'Aaron_Rodgers',
       'accuracy': 98,
       'is_old': true,
+      'style_id': 2,
   },
   {
       'name': 'Patrick_Mahomes',
       'accuracy': 97,
       'is_old': false,
+      'style_id': 2,
   },
   {
       'name': 'Russel_Wilson',
       'accuracy': 96,
       'is_old': true,
+      'style_id': 3,
   },
   {
       'name': 'Josh_Allen',
       'accuracy': 91,
       'is_old': false,
+      'style_id': 4,
   },
   {
       'name': 'Drew_Brees',
       'accuracy': 93,
       'is_old': true,
+      'style_id': 1,
   },
 ];
 
